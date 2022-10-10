15112e565c9bc99c6e523887309476f8ad02538b
multityy <59746858+jip-erk@users.noreply.github.com>
2022-10-10 22:14:19 +0200

    hhhh

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,9 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   // ssr: false,
 
-  // server: {
-  //   host: '0.0.0.0', // default: localhost
-  // },
+  server: {
+    host: '0.0.0.0', // default: localhost
+  },
 
   generate: {
     fallback: true,
