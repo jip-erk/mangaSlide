634560e7b0581d76ef4e6b270c260fe867074117
multityy <59746858+jip-erk@users.noreply.github.com>
2022-10-10 19:52:54 +0200

    server+

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   // ssr: false,
+
   server: {
     host: '0.0.0.0', // default: localhost
   },
@@ -10,7 +11,7 @@ export default {
   },
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
